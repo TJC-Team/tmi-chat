6fb0ff129143789cca8213252d0f3c11995d120b
Kai Schallenberg <hello@kaischallenberg.de>
2020-06-27 15:57:36 +0200

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,18 @@
+// https://dev.twitch.tv/console/apps => Anwendung erstellen => ID ClientID
+var krakenClientID = '';
+
+
+// Channels, die angezeigt werden sollen
+var stream_channel = ['thejocraft_live'];
+
+
+// Nachrichten vom User werden nicht angezeigt
+var chatbotname = "tjc_bot"; 
+
+
+// Nachrichten mit Prefix werden nicht angezeigt
+var chatprefix = "!"; 
+
+
+// Partikel in Verbindung mit https://github.com/tjc-team/Hypemeeter-Front
+var hype_activate = false; 
